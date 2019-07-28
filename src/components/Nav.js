39e6b5293775268d2f9b055756501411b8b46ef7
hideMan <thanhdntage@gmail.com>
2019-07-28 12:57:07 +0700

    layout nav

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../App.css';
+
+export default function Nav() {
+    return (
+        <nav className="Nav-main">
+            <h2>Logo</h2>
+            <ul>
+                <li>Shop</li>
+                <li>Service</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    );
+}
